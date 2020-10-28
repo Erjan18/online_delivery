9a133fe57557cacc40438e946d093c4f272fbc72
Erjan18 <johnydepp2002@mail.ru>
2020-10-28 19:10:58 +0600

    urls

--- online_store/store/urls.py
+++ online_store/store/urls.py
@@ -4,7 +4,10 @@ from .views import *
 
 
 urlpatterns = [
-   path('',home_page),
-   path('products/',products_page),
-   path('custumer/',costumer_page),
+   path('',home_page,name='home'),
+   path('products/',products_page,name='products'),
+   path('customer/<int:pk>/',costumer_page,name='customer_page'),
+   path('create_order/',create_order,name='create_order'),
+   path('update_order/<int:pk>/',update_order,name='update_order'),
+   path('delete_order/<int:pk>/',delete_order,name='delete_order')
 ]
