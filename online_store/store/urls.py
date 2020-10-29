f8e410161831be4257082216d0406654024db591
Erjan18 <johnydepp2002@mail.ru>
2020-10-29 19:08:18 +0600

    add register

--- online_store/store/urls.py
+++ online_store/store/urls.py
@@ -5,9 +5,10 @@ from .views import *
 
 urlpatterns = [
    path('',home_page,name='home'),
+   path('register/',register,name='register'),
    path('products/',products_page,name='products'),
    path('customer/<int:pk>/',costumer_page,name='customer_page'),
-   path('create_order/',create_order,name='create_order'),
+   path('create_order/<int:pk>/',create_order,name='create_order'),
    path('update_order/<int:pk>/',update_order,name='update_order'),
    path('delete_order/<int:pk>/',delete_order,name='delete_order')
 ]
