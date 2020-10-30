c1c918d5ad14def7453a57351acf100afca03365
erjan <johnydepp2002@mail.ru>
2020-10-30 20:38:40 +0600

    add CustomerRegisterForm

--- online_store/store/forms.py
+++ online_store/store/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from  .models import Order
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class Orderform(ModelForm):
 
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CustomerRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
