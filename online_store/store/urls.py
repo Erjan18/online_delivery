c4e625ef981ece0d5fc3f4db36583064d59ddcc7
erjan <johnydepp2002@mail.ru>
2020-10-30 20:41:49 +0600

    add login and logout

--- online_store/store/urls.py
+++ online_store/store/urls.py
@@ -6,9 +6,11 @@ from .views import *
 urlpatterns = [
    path('',home_page,name='home'),
    path('register/',register,name='register'),
+   path('login/',login_page,name='login'),
+   path('logout/',logout_page,name='logout'),
    path('products/',products_page,name='products'),
    path('customer/<int:pk>/',costumer_page,name='customer_page'),
    path('create_order/<int:pk>/',create_order,name='create_order'),
    path('update_order/<int:pk>/',update_order,name='update_order'),
-   path('delete_order/<int:pk>/',delete_order,name='delete_order')
+   path('delete_order/<int:pk>/',delete_order,name='delete_order'),
 ]
