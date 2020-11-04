53013d3f8e069cf535ac24b3b9a16851d3353e56
erjan <johnydepp2002@mail.ru>
2020-11-04 19:34:15 +0600

    adds class UserProfile

--- online_store/store/forms.py
+++ online_store/store/forms.py
@@ -1,9 +1,18 @@
 from django.forms import ModelForm
-from  .models import Order
+from .models import Order, Customer
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-class Orderform(ModelForm):
 
+
+
+
+class UserProfile(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user',]
+
+class Orderform(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
