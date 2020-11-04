e648ec24cb4e188ad22d8005e0282c7b9b9569e1
erjan <johnydepp2002@mail.ru>
2020-11-04 16:50:24 +0600

    adds ALLOWED_HOSTS = ['*']

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3vp(ahz0vjt==feh5#9g60offh8tgqcifbwn8v83e@y$6$!r@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
