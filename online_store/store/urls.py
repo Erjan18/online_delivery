1682162199e0712780a34400d2ed52427040c2fb
erjan <johnydepp2002@mail.ru>
2020-11-05 19:22:09 +0600

    added links:youtube,reset_password,reset_password_done,reset/<uidb64>/<token>,reset_password_complete

--- online_store/store/urls.py
+++ online_store/store/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import *
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -9,6 +10,11 @@ urlpatterns = [
    path('user/', user_page,name= 'user'),
    path('login/',login_page,name='login'),
    path('settings/',account_settings, name ='settings'),
+   path('youtube/',youtube,name = 'youtube'),
+   path('reset_password/',auth_views.PasswordResetView.as_view(template_name='store/reset_password.html'),name='password_reset'),
+   path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name='store/reset_password_done.html'), name='password_reset_done'),
+   path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='store/reset_password_form.html'), name='password_reset_confirm'),
+   path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='store/reset_password_send.html'), name='password_reset_complete'),
    path('logout/',logout_page,name='logout'),
    path('products/',products_page,name='products'),
    path('customer/<int:pk>/',costumer_page,name='customer_page'),
